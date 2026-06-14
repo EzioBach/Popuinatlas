2a28a8c18c8393b31269251c0173201a0d42a4e6
EzioBach <201830596+EzioBach@users.noreply.github.com>
2026-06-14 18:50:37 +0200

    Add user_id initialization in session state
    Initialize user_id in session state if not present.

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -5,6 +5,9 @@ import streamlit as st
 
 from utils import set_theme, sidebar_header, load_user, progress_label
 
+if "user_id" not in st.session_state:
+    st.session_state["user_id"] = ""
+
 set_theme()
 user_id = sidebar_header()
 
